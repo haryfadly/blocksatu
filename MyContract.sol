f92a41ccee492d3050d00fd9529bee0bb9e56eec
Remix Workspace <>
2022-11-09 13:14:32 +0700

    add SafeMath

--- MyContract.sol
+++ MyContract.sol
@@ -1,71 +1,16 @@
 pragma solidity 0.5.1;
 
+import "./Math.sol";
+
+// using DRY: dont repeat yourself
 contract MyContract {
-    // string public value = "myValue";
-    // string public constant value = "myValue";
-    // bool public myBool = true;
-    // int public myInt = -1;
-    // uint public myUint = 1;
-    // uint8 public myUint8 = 8;
-    // uint256 public myUint256 = 9999;
+    // ..
+    uint256 public value;
 
-    // constructor () public {
-    //    value = "myValue";
-    //    }
+    function calculate(uint _value1, uint _value2) public {
+        // ..
+        // value = _value1 / _value2;
+        value = Math.divide(_value1, _value2); 
 
-    // function get () public view returns (string){
-    //    return value;
-    //    }
-
-    // function set(string memory _value) public {
-    //     value = _value;
-    //     }
-    //    enum State{Waiting, Ready, Active}
-    //    State public state;
-
-    //    constructor () public {
-    //        state = State.Waiting;
-    //    }
-    //    function activate () public {
-    //        state = State.Active;
-    //    }
-    //    function isActive () public view returns(bool){
-    //        return state == State.Active;
-    //    }
-
-    // Person[] public people;
-    uint256 public peopleCount = 0;
-    mapping(uint256 => Person) public people;
-
-    uint256 openingTime;
-
-    modifier onlyWhileOpen() {
-        require(block.timestamp >= openingTime);
-        _;
-    }
-
-    struct Person {
-        uint256 _id;
-        string _firstName;
-        string _lastName;
-    }
-
-    constructor() public {
-        openingTime = 1667878939; //update this value
-    }
-
-    function addPerson(
-        string memory _firstName, 
-        string memory _lastName)
-        public
-        onlyWhileOpen
-    {
-        incrementCount();
-        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
-        // people.push(Person(_firstName, _lastName));
-    }
-
-    function incrementCount() internal {
-        peopleCount += 1;
     }
 }
